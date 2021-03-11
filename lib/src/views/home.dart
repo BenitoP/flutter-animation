d846577026d9378c60169ec1fd40a41db50a67d8
Benito Pellencin <benito@iambenito.co.za>
2021-03-11 17:39:02 +0200

    first commit

---
+++ lib/src/views/home.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+import 'dart:math';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  Animation<double> boxAnimation;
+  AnimationController boxController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    boxController =
+        AnimationController(duration: Duration(milliseconds: 200), vsync: this);
+
+    boxAnimation = Tween(
+      begin: pi * 0.6,
+      end: pi * 0.62,
+    ).animate(CurvedAnimation(
+      parent: boxController,
+      curve: Curves.easeInOut,
+    ));
+
+    boxAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+
+    boxController.forward();
+
+    catController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: -35.0, end: -80.0).animate(CurvedAnimation(
+      parent: catController,
+      curve: Curves.easeIn,
+    ));
+  }
+
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+      boxController.forward();
+    } else if (catController.status == AnimationStatus.dismissed) {
+      catController.forward();
+      boxController.stop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation'),
+      ),
+      body: GestureDetector(
+        onTap: onTap,
+        child: Center(
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              buildCatAnimation(),
+              buildBox(),
+              buildLeftFlap(),
+              buildRightFlap(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (BuildContext context, child) {
+        return Positioned(
+          child: child,
+          top: catAnimation.value,
+          right: 0.0,
+          left: 0.0,
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      height: 200.0,
+      width: 200,
+      color: Colors.brown,
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned(
+      left: 1.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: boxAnimation.value,
+            alignment: Alignment.topLeft,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      right: 1.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: -boxAnimation.value,
+            alignment: Alignment.topRight,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+}
